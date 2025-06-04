e6a5281fe555e4d98873845c3317101ce4a6919f
Rohan Mathew <robozmat@gmail.com>
2025-06-04 16:23:48 +0100

    added locate and time

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from time import time as now
 import os
+from trailrouter import find
+from poi_enricher import *
 
 app = Flask(__name__)
+
+def coord_http_to_py(s):
+    return (s["lat"], s["long"])
+def coord_py_to_http(s):
+    return {"lat": s[0], "long": s[1]}
 
 @app.route("/test")
 def test():
@@ -10,6 +17,36 @@ def test():
         "timestamp": now(),
         "msg": "yes, your request reached me"
     }
+    
+@app.route("/time", methods = ["POST"])
+def time():
+    inp = request.json
+    start = coord_http_to_py(inp["start"])
+    end = coord_http_to_py(inp["end"])
+    route = find([start, end])["route"][0]
+    
+    return {
+        "time": round(route["duration"] / 1000 / 60)
+    }
+    
+@app.route("/locate", methods = ["POST"])
+def locate():
+    inp = request.json
+    attempt = inp["name"]
+    return coord_py_to_http(geocode_place_name(attempt))
+    
+@app.route("/longroute", methods = ["POST"])
+def longroute():
+    inp = request.json
+    start = coord_http_to_py(inp["start"])
+    end = coord_http_to_py(inp["end"])
+    total_time = inp["total_time"]
+    AVERAGE_WALKING_SPEED = 4.8
+    
+    return enrich_routes_with_location_info(
+        [start, end],
+        target_distance=AVERAGE_WALKING_SPEED*total_time/60
+    )
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,69 @@
+import googlemaps as maps
+from polyline import decode
+
+KEY = "AIzaSyB1Mhqz0NlzTycRp0tTfQIJoexp9_rWQiA"
+START = (51.49142612816029, -0.20781661388698155)
+END = (51.49935665314372, -0.17955355472110562)
+COMBO = (START, END)
+
+def coord_to_gm(coord):
+    return f"{coord[0]},{coord[1]}"
+
+cli = maps.Client(key=KEY)
+
+output = cli.directions(coord_to_gm(START), coord_to_gm(END), mode="transit")
+route = output[0]
+decoded_poly = decode(route["overview_polyline"]["points"], precision=6)
+
+leg = route["legs"][0]
+
+steps = leg["steps"]
+
+
+def full_points(s):
+    start = (s["start_location"]["lat"], s["start_location"]["lng"])
+    end = (s["end_location"]["lat"], s["end_location"]["lng"])
+    points = decode(s["polyline"]["points"])
+    points.insert(0, start)
+    points.append(end)
+    
+    return points
+
+class Start:
+    pass
+
+class Walk:
+    def __init__(self, raw):
+        self.distance = s["distance"]["value"]
+        self.duration = s["duration"]["value"]
+        self.points = full_points(s)
+        
+    def append(self, other):
+        self.distance += other.distance
+        self.duration += other.duration
+        self.points.extend(other.points)
+        
+class Transit:
+    def __init__(self, raw):
+        self.distance = s["distance"]["value"]
+        self.duration = s["duration"]["value"]
+        self.points = full_points(s)
+        self.line = s["transit_details"]["line"]["name"]
+
+
+# summarise 
+newsteps = [Start()]
+
+for s in steps:    
+    if s["travel_mode"] == "WALKING":
+        s = Walk(s)
+        
+        if newsteps[-1] is Walk:
+            newsteps[-1].append(s)
+        else:
+            newsteps.append(s)
+            
+    if s["travel_mode"] == "TRANSIT":
+        s = Transit(s)
+        
+        if new
