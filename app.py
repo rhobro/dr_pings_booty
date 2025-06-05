1a51d5a14df5c54a9d247c43a97f0ad85eb6ffa7
Rohan Mathew <robozmat@gmail.com>
2025-06-05 12:45:38 +0100

    cors enable

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from time import time as now
 import os
 from trailrouter import find
 from poi_enricher import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def coord_http_to_py(s):
     return (s["lat"], s["long"])
@@ -12,6 +15,7 @@ def coord_py_to_http(s):
     return {"lat": s[0], "long": s[1]}
 
 @app.route("/test")
+@cross_origin()
 def test():
     return {
         "timestamp": now(),
@@ -19,12 +23,12 @@ def test():
     }
     
 @app.route("/time", methods = ["POST"])
+@cross_origin()
 def time():
     inp = request.json
-    start = coord_http_to_py(inp["start"])
-    end = coord_http_to_py(inp["end"])
+    coords = [coord_http_to_py(c) for c in inp["coords"]]
     
-    result = find([start, end], target_distance=1)    
+    result = find(coords, target_distance=1)    
     route = result["routes"][0]
     
     return {
@@ -32,26 +36,27 @@ def time():
     }
     
 @app.route("/locate", methods = ["POST"])
+@cross_origin()
 def locate():
     inp = request.json
     attempt = inp["name"]
     return coord_py_to_http(geocode_place_name(attempt))
 
 @app.route("/routefmt", methods = ["POST"])
+@cross_origin()
 def routefmt():
     inp = request.json
-    start = coord_http_to_py(inp["start"])
-    end = coord_http_to_py(inp["end"])
+    coords = [coord_http_to_py(c) for c in inp["coords"]]
     total_time = inp["total_time"]
     fmt = inp["fmt"]  # json | gpx | kml
     
-    return find([start, end], total_time=total_time, output=fmt)
+    return find(coords, total_time=total_time, output=fmt)
     
 @app.route("/longroute", methods = ["POST"])
+@cross_origin()
 def longroute():
     inp = request.json
-    start = coord_http_to_py(inp["start"])
-    end = coord_http_to_py(inp["end"])
+    coords = [coord_http_to_py(c) for c in inp["coords"]]
     total_time = inp["total_time"]
     AVERAGE_WALKING_SPEED = 4.8
     
@@ -60,7 +65,7 @@ def longroute():
     
     try:
         enriched_data = enrich_routes_with_location_info(
-            [start, end],
+            coords,
             target_distance=target_distance_meters,
             green_preference=1.0,     
             hills_preference=0.0,
